036a34bee08791ac6bc9b2184b426dc686762360
Samuel Tober <samueltober@gmail.com>
2024-02-17 22:41:17 +0100

    build simple browser class

--- URL.py
+++ URL.py
@@ -1,12 +1,24 @@
+import socket
+import ssl
+
 class URL:
 	def __init__(self, url): 
 		self.scheme, url = url.split("://", 1)
-		assert self.scheme == "http"
+		assert self.scheme in ["http", "https"]
 		if "/" not in url: 
 			url = url + "/"
 
 		self.host, url = url.split("/", 1)
 		self.path = "/" + url
+
+		if self.scheme == 'http':
+			self.port = 80
+		elif self.scheme == 'https':
+			self.port = 443
+
+		if ":" in self.host: 
+			self.host, port = self.host.split(":", 1)
+			self.port = int(port)
 
 	def request(self):
 		s = socket.socket(
@@ -14,34 +26,29 @@ class URL:
 			type=socket.SOCK_STREAM,
 			proto=socket.IPPROTO_TCP,
 		)
-		s.connect((self.host, 80))
-		        s.send(("GET {} HTTP/1.0\r\n".format(self.path) + \
-                "Host: {}\r\n\r\n".format(self.host)) \
-               .encode("utf8"))
+		if self.scheme == 'https':
+			ctx = ssl.create_default_context()
+			s = ctx.wrap_socket(s, server_hostname=self.host)
+		s.connect((self.host, self.port))
+		s.send(("GET {} HTTP/1.0\r\n".format(self.path) + \
+		"Host: {}\r\n\r\n".format(self.host)) \
+		.encode("utf8"))
+
 		response = s.makefile("r", encoding="utf8", newline="\r\n")
 		statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
-        response_headers = {}
-        while True:
-        	line = response.readline()
-        	if line == "\r\n": break
-        	header, value = line.split(":", 1)
-        	response_headers[header.casefold()] = value.strip()
+		version, status, explanation = statusline.split(" ", 2)
+		response_headers = {}
+		body = response.read()
 
-        	assert "transfer-encoding" not in response_headers
-        	assert "content-encoding" not in response_headers
+		while True:
+			line = response.readline()
+			if line == "\r\n" or line == "": break
+			header, value = line.split(":", 1)
+			response_headers[header.casefold()] = value.strip()
 
-        	body = response.read()
-        	s.close()
+			assert "transfer-encoding" not in response_headers
+			assert "content-encoding" not in response_headers
 
-        return body
+			s.close()
 
-    def show(body):
-	    in_tag = False
-	    for c in body:
-	        if c == "<":
-	            in_tag = True
-	        elif c == ">":
-	            in_tag = False
-	        elif not in_tag:
-	            print(c, end="")
+		return body
---
+++ browser.py
@@ -0,0 +1,23 @@
+from URL import URL
+
+class Browser:
+	def __init__(self):
+		pass
+
+	def show(self, body):
+		in_tag = False
+		for c in body:
+			if c == "<":
+				in_tag = True
+			elif c == ">":
+				in_tag = False
+			elif not in_tag:
+				print(c, end="")
+
+	def load(self, url):
+		body = url.request()
+		self.show(body)
+
+if __name__ == "__main__":
+	browser = Browser()
+	browser.load(URL("https://browser.engineering/examples/example1-simple.html"))
