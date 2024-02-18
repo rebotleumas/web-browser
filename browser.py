035fc5585d783c72d95749ed21103d6d95b28789
Samuel Tober <samueltober@gmail.com>
2024-02-18 21:02:52 +0100

    add support for lt and gt entities

--- browser.py
+++ browser.py
@@ -2,17 +2,29 @@ from URL import URL
 
 class Browser:
 	def __init__(self):
-		pass
+		self.entities = {"lt": "<", "gt": ">"}
 
 	def show(self, body):
 		in_tag = False
-		for c in body:
-			if c == "<":
+		idx = 0
+		while idx < len(body):
+			char = body[idx]
+
+			if char == "&":
+				entity = ""
+				idx += 1
+				while body[idx] != ";":
+					entity += body[idx]
+					idx += 1
+				assert entity in self.entities
+				print(self.entities[entity], end="")
+			elif char == "<":
 				in_tag = True
-			elif c == ">":
+			elif char == ">":
 				in_tag = False
 			elif not in_tag:
-				print(c, end="")
+				print(char, end="")
+			idx += 1
 
 	def load(self, url):
 		body = url.request()
@@ -20,4 +32,4 @@ class Browser:
 
 if __name__ == "__main__":
 	browser = Browser()
-	browser.load(URL("data:text/html, <div>Hello world</div>"))
+	browser.load(URL("data:text/html, <div>Hello&lt;div&gt; world</div>"))
