ecf779be9c155954f2a568b37b86ecd79320cf2a
Samuel Tober <samueltober@gmail.com>
2024-02-24 16:22:18 +0100

    only draw visible chars

--- src/browser/browser.py
+++ src/browser/browser.py
@@ -58,6 +58,8 @@ class Browser:
     def draw(self):
     	self.canvas.delete("all")
     	for x, y, c in self.display_list:
+    		if y - self.scroll > HEIGHT: continue
+    		if y < VSTEP - self.scroll: continue
     	    self.canvas.create_text(x, y - self.scroll, text=c)
 
     def layout(self, text):
