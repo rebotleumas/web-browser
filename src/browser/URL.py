7c42170eebbf4a8b4d2675d68778fcbdb2c988b4
Samuel Tober <samueltober@gmail.com>
2024-02-24 14:51:14 +0100

    support redirects

--- src/browser/URL.py
+++ src/browser/URL.py
@@ -1,97 +1,122 @@
 import socket
 import ssl
 
+
 class URL:
-	def __init__(self, url="file://C:/Users/samue/Documents/Projects/WebBrowser/test_file.txt"): 
-		self.scheme, new_url = url.split(":", 1)
-		assert self.scheme in ["http", "https", "file", "data", "view-source"]
+    def __init__(
+        self, url="file://C:/Users/samue/Documents/Projects/WebBrowser/test_file.txt"
+    ):
+        self.scheme, new_url = url.split(":", 1)
+        assert self.scheme in ["http", "https", "file", "data", "view-source"]
 
-		if self.scheme in ["http", "https"]:
-			self.parse_http(url)
+        if self.scheme in ["http", "https"]:
+            self.parse_http(url)
 
-		if self.scheme == "file":
-			_, self.path = url.split("://")
+        if self.scheme == "file":
+            _, self.path = url.split("://")
 
-		if self.scheme == "data":
-			self.mediatype, self.html_data = new_url.split(",", 1)
-			assert self.mediatype == "text/html"
+        if self.scheme == "data":
+            self.mediatype, self.html_data = new_url.split(",", 1)
+            assert self.mediatype == "text/html"
 
-		if self.scheme == "view-source":
-			self.parse_http(new_url)
+        if self.scheme == "view-source":
+            self.parse_http(new_url)
 
-		self.socket_cache = {}
+        self.socket_cache = {}
+        self.redirects = 0
+        self.MAX_REDIRECTS = 5
 
-	def parse_http(self, url):
-		scheme, url = url.split("://", 1)
-		if "/" not in url:
-			url = url + "/"
+    def parse_http(self, url):
+        scheme, url = url.split("://", 1)
+        if "/" not in url:
+            url = url + "/"
 
-		self.host, url = url.split("/", 1)
-		self.path = "/" + url
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
 
-		if scheme == "http":
-			self.port = 80
-		elif scheme == "https":
-			self.port = 443
+        if scheme == "http":
+            self.port = 80
+        elif scheme == "https":
+            self.port = 443
 
-		if ":" in self.host: 
-			self.host, port = self.host.split(":", 1)
-			self.port = int(port)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
 
-	def request(self) -> str:
-		body = ''
-		if self.scheme == "file":
-			try:
-				with open(self.path) as f: body = f.read()
-			except (FileNotFoundError, IOError):
-				return 'Incorrect file or non-existent file path!'
-		elif self.scheme == "data":
-			body = self.html_data
-		else:
-			request_headers = {'Host': self.host, 'User-Agent': 'Bowser'}
-			request = f"GET {self.path} HTTP/1.0\r\n"
-			for header, header_value in request_headers.items():
-				request += f"{header}: {header_value}\r\n"
-			request += "\r\n\r\n"
+    def request(self) -> str:
+        body = ""
+        if self.scheme == "file":
+            try:
+                with open(self.path) as f:
+                    body = f.read()
+            except (FileNotFoundError, IOError):
+                return "Incorrect file or non-existent file path!"
+        elif self.scheme == "data":
+            body = self.html_data
+        else:
+            request_headers = {
+                "Host": self.host,
+                "User-Agent": "Bowser",
+                "Connection": "Keep-Alive",
+                "Keep-Alive": "timeout=1, max=1",
+            }
+            request = f"GET {self.path} HTTP/1.0\r\n"
+            for header, header_value in request_headers.items():
+                request += f"{header}: {header_value}\r\n"
+            request += "\r\n\r\n"
 
-			if (self.host + str(self.port)) in self.socket_cache:
-				s = self.socket_cache[self.host + str(self.port)]
-			else:
-				s = socket.socket(
-					family=socket.AF_INET,
-					type=socket.SOCK_STREAM,
-					proto=socket.IPPROTO_TCP,
-				)
-				if self.scheme == 'https':
-					ctx = ssl.create_default_context()
-					s = ctx.wrap_socket(s, server_hostname=self.host)
+            if (self.host + str(self.port)) in self.socket_cache:
+                s = self.socket_cache[self.host + str(self.port)]
+            else:
+                s = socket.socket(
+                    family=socket.AF_INET,
+                    type=socket.SOCK_STREAM,
+                    proto=socket.IPPROTO_TCP,
+                )
+                if self.scheme == "https":
+                    ctx = ssl.create_default_context()
+                    s = ctx.wrap_socket(s, server_hostname=self.host)
 
-				s.connect((self.host, self.port))
-				self.socket_cache[self.host + str(self.port)] = s
-			s.send(request.encode('utf8'))
+                s.connect((self.host, self.port))
+                self.socket_cache[self.host + str(self.port)] = s
 
-			response = s.makefile("r", encoding="utf8", newline="\r\n")
+            s.send(request.encode("utf8"))
+            response = s.makefile("r", encoding="utf8", newline="\r\n")
 
-			return response
+            return self.parse_response(response)
 
-	def parse_response(self, response):
-		statusline = response.readline()
-		version, status, explanation = statusline.split(" ", 2)
-		response_headers = {}
-		while True:
-			line = response.readline()
-			if line == "\r\n": break
-			header, value = line.split(":", 1)
-			response_headers[header.casefold()] = value.strip()
+    def parse_response(self, response):
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
 
-			assert "transfer-encoding" not in response_headers
-			assert "content-encoding" not in response_headers
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
 
-		content_length = int(response_headers['content-length'])
+            assert "transfer-encoding" not in response_headers
+            assert "content-encoding" not in response_headers
 
-		body = response.read()
-		if self.scheme == "view-source":
-			body.replace("<", "&lt;")
-			body.replace(">", "&gt;")
+        if 300 <= int(status) < 400:
+            assert "location" in response_headers
+            self.redirects += 1
+            assert self.redirects <= self.MAX_REDIRECTS
+            redirect_url = response_headers["location"]
+            self.scheme, _ = redirect_url.split(":", 1)
+            self.parse_http(redirect_url)
+            return self.request()
+        else:
+            content_length = (
+                int(response_headers["content-length"])
+                if "content-length" in response_headers
+                else -1
+            )
+            body = response.read()
+            if self.scheme == "view-source":
+                body.replace("<", "&lt;")
+                body.replace(">", "&gt;")
 
-		return body[:content_length]
+            return body[:content_length]
--- src/browser/browser.py
+++ src/browser/browser.py
@@ -1,36 +1,40 @@
 from URL import URL
 
+
 class Browser:
-	def __init__(self):
-		self.entities = {"lt": "<", "gt": ">"}
+    def __init__(self):
+        self.entities = {"lt": "<", "gt": ">"}
 
-	def show(self, body):
-		in_tag = False
-		idx = 0
-		while idx < len(body):
-			char = body[idx]
+    def show(self, body):
+        in_tag = False
+        idx = 0
+        while idx < len(body):
+            char = body[idx]
 
-			if char == "&":
-				entity = ""
-				idx += 1
-				while body[idx] != ";":
-					entity += body[idx]
-					idx += 1
-				assert entity in self.entities
-				print(self.entities[entity], end="")
-			elif char == "<":
-				in_tag = True
-			elif char == ">":
-				in_tag = False
-			elif not in_tag:
-				print(char, end="")
-			idx += 1
+            if char == "&":
+                entity = ""
+                idx += 1
+                while body[idx] != ";":
+                    entity += body[idx]
+                    idx += 1
+                    if idx >= len(body): 
+                    	print(entity, end="")
+                    	break
+                if entity in self.entities:
+                    print(self.entities[entity], end="") 
+            elif char == "<":
+                in_tag = True
+            elif char == ">":
+                in_tag = False
+            elif not in_tag:
+                print(char, end="")
+            idx += 1
 
-	def load(self, url):
-		response = url.request()
-		body = url.parse_response(response)
-		self.show(body)
+    def load(self, url):
+        body = url.request()
+        self.show(body)
+
 
 if __name__ == "__main__":
-	browser = Browser()
-	browser.load(URL("https://example.org/"))
+    browser = Browser()
+    browser.load(URL("http://browser.engineering/redirect"))
